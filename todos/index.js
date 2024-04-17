b29f1b7c853e90747ce58e0baa04c64fda3238e9
HussainBinFaisal <160384893+HussainBinFaisal@users.noreply.github.com>
2024-04-17 18:47:07 +0500

    Add files via upload

---
+++ todos/index.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let task = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What do you want to add to the list/TODOS?",
+        },
+        {
+            name: "ADDMORE",
+            type: 'confirm',
+            message: "Would you like to add more Tasks?",
+            default: "false",
+        },
+    ]);
+    todos.push(task.todo);
+    condition = task.ADDMORE;
+    console.log(todos);
+}
